76a052c9842c94053b36d974ce1941124127bf43
Oreo <lala21525@gmail.com>
2015-04-30 23:42:21 +0800

    Initial Commit

---
+++ HW-Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW-Note
+//
+//  Created by 李育豪 on 2015/4/30.
+//  Copyright (c) 2015年 ALPHACamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HW-Note/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW-Note
+//
+//  Created by 李育豪 on 2015/4/30.
+//  Copyright (c) 2015年 ALPHACamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
